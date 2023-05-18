b8aa256fcd0aa4f3df78bdfe07f8f494da43d770
PaXan <116594902+Paxanchik@users.noreply.github.com>
2023-05-18 12:48:02 +0300

    Add files via upload

---
+++ memory_simulator/memory_simulator/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace memory_simulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.numUpDownTests = new System.Windows.Forms.NumericUpDown();
+            this.numUpDownDigits = new System.Windows.Forms.NumericUpDown();
+            this.numUpDownInterval = new System.Windows.Forms.NumericUpDown();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBarAttempts = new System.Windows.Forms.ProgressBar();
+            this.progressBarCorrect = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownTests)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownDigits)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownInterval)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numUpDownTests
+            // 
+            this.numUpDownTests.Location = new System.Drawing.Point(437, 24);
+            this.numUpDownTests.Name = "numUpDownTests";
+            this.numUpDownTests.Size = new System.Drawing.Size(120, 20);
+            this.numUpDownTests.TabIndex = 0;
+            // 
+            // numUpDownDigits
+            // 
+            this.numUpDownDigits.Location = new System.Drawing.Point(437, 60);
+            this.numUpDownDigits.Name = "numUpDownDigits";
+            this.numUpDownDigits.Size = new System.Drawing.Size(120, 20);
+            this.numUpDownDigits.TabIndex = 1;
+            // 
+            // numUpDownInterval
+            // 
+            this.numUpDownInterval.Location = new System.Drawing.Point(437, 96);
+            this.numUpDownInterval.Name = "numUpDownInterval";
+            this.numUpDownInterval.Size = new System.Drawing.Size(120, 20);
+            this.numUpDownInterval.TabIndex = 2;
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblNumber.Location = new System.Drawing.Point(257, 189);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(46, 18);
+            this.lblNumber.TabIndex = 3;
+            this.lblNumber.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBarAttempts
+            // 
+            this.progressBarAttempts.Location = new System.Drawing.Point(75, 281);
+            this.progressBarAttempts.Name = "progressBarAttempts";
+            this.progressBarAttempts.Size = new System.Drawing.Size(389, 23);
+            this.progressBarAttempts.TabIndex = 4;
+            // 
+            // progressBarCorrect
+            // 
+            this.progressBarCorrect.Location = new System.Drawing.Point(75, 310);
+            this.progressBarCorrect.Name = "progressBarCorrect";
+            this.progressBarCorrect.Size = new System.Drawing.Size(389, 23);
+            this.progressBarCorrect.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(156, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 31);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(314, 348);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 31);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Проверить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(301, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 18);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Колл-во опросов";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(301, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Интервал с";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(301, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Колл-во цифр";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(437, 135);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(100, 20);
+            this.tbUsername.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(301, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 18);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Имя";
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(239, 239);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(100, 20);
+            this.tbInput.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(153, 241);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 18);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Результат";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(587, 391);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBarCorrect);
+            this.Controls.Add(this.progressBarAttempts);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.numUpDownInterval);
+            this.Controls.Add(this.numUpDownDigits);
+            this.Controls.Add(this.numUpDownTests);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Memory simulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownTests)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownDigits)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownInterval)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numUpDownTests;
+        private System.Windows.Forms.NumericUpDown numUpDownDigits;
+        private System.Windows.Forms.NumericUpDown numUpDownInterval;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBarAttempts;
+        private System.Windows.Forms.ProgressBar progressBarCorrect;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ memory_simulator/memory_simulator/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace memory_simulator
+{
+    public partial class Form1 : Form
+    {
+        private int numTests;
+        private int numDigits;
+        private int timeInterval;
+        private int currentTest;
+        private int correctAnswers;
+        private int incorrectAnswers;
+        private string targetNumber;
+        private string userNumber;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Обработчик события нажатия на кнопку "Старт"
+            numTests = (int)numUpDownTests.Value;
+            numDigits = (int)numUpDownDigits.Value;
+            timeInterval = (int)numUpDownInterval.Value;
+            ClearResults();
+            currentTest = 0;
+            correctAnswers = 0;
+            incorrectAnswers = 0;
+            progressBarAttempts.Maximum = numTests;
+            progressBarCorrect.Maximum = numTests;
+
+            timer1.Interval = timeInterval * 1000; // Преобразуем интервал в миллисекунды
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Задаем значения по умолчанию
+            numTests = 5;
+            numDigits = 5;
+            timeInterval = 5;
+            currentTest = 0;
+            correctAnswers = 0;
+            incorrectAnswers = 0;
+
+            // Запускаем датчик и очищаем компоненты Label и Edit
+            GenerateRandomNumber();
+            ClearResults();
+        }
+        private void GenerateRandomNumber()
+        {
+            // Генерируем случайное число заданного количества цифр
+            Random random = new Random();
+            targetNumber = "";
+            for (int i = 0; i < numDigits; i++)
+            {
+                targetNumber += random.Next(0, 9).ToString();
+            }
+        }
+        private void ClearResults() 
+        {
+            // Очищаем результаты предыдущего теста
+            lblNumber.Text = "";
+            userNumber = "";
+            progressBarAttempts.Value = 0;
+            progressBarCorrect.Value = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Обработчик события таймера, отображающий случайное число в Label
+            timer1.Interval = timeInterval * 1000;
+            lblNumber.Text = targetNumber;
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Обработчик события нажатия на кнопку "Отправить"
+            userNumber = tbInput.Text.Trim();
+
+            if (userNumber == targetNumber)
+            {
+                correctAnswers++;
+            }
+            else
+            {
+                incorrectAnswers++;
+            }
+
+            currentTest++;
+            progressBarAttempts.Value = currentTest;
+            progressBarCorrect.Value = correctAnswers;
+            progressBarAttempts.Value = 2;
+            ClearResults();
+            tbInput.Text = "";
+
+            if (currentTest >= numTests)
+            {
+                // Тестирование завершено
+                double accuracy = (double)correctAnswers / numTests * 100;
+                string username = tbUsername.Text.Trim();
+                SaveResultsToFile(username, accuracy);
+
+                MessageBox.Show($"Тестирование завершено!\n\nИмя: {username}\nПроцент качества памяти: {accuracy.ToString("0.00")}%");
+
+                // Сбрасываем значения по умолчанию
+                numTests = 5;
+                numDigits = 3;
+                timeInterval = 5;
+
+                // Очищаем компоненты Label и Edit
+                lblNumber.Text = "";
+                tbInput.Text = "";
+                tbUsername.Text = "";
+            }
+            else
+            {
+                GenerateRandomNumber();
+            }
+        }
+        private void SaveResultsToFile(string username, double accuracy)
+        {
+            // Сохраняем результаты тестирования в файл
+            string fileName = "MemoryTestResults.txt";
+            using (StreamWriter writer = File.AppendText(fileName))
+            {
+                writer.WriteLine($"Имя: {username}");
+                writer.WriteLine($"Процент качества памяти: {accuracy.ToString("0.00")}%");
+                writer.WriteLine();
+            }
+        }
+    }
+}
